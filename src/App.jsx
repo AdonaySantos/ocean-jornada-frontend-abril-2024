bf9141e047d416d613521de1549c1501264d0cad
Adonay Santos <adonay.rssantos@gmail.com>
2024-04-19 00:02:52 +0000

    animação da sombra do card

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,16 @@ function App() {
     <>
       <div className="card">
         <h2>Rick Sanchez</h2>
+        <p>Status: Vivo</p>
+        <p>Espécie: Humana</p>
+        <p>Origem: Terra C-137</p>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
       </div>
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
+      </div>
+      
     </>
   )
 }
