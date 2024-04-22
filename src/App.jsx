1ede5546a608614b0f68950a39109f783cb3b88e
Adonay Santos <adonay.rssantos@gmail.com>
2024-04-22 02:14:04 +0000

    Desafio feito

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ function App() {
   return (
     <>
       <div className="card">
+        <h2 className='title'></h2>
         <h2>Rick Sanchez</h2>
-        <p>Status: Vivo</p>
-        <p>Espécie: Humana</p>
-        <p>Origem: Terra C-137</p>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
+        <div className='tags'>
+          <p class="status">Status: Vivo</p>
+          <p class="status">Espécie: Humana</p>
+          <p class="status">Origem: Terra C-137</p>
+        </div>
+      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
       </div>
       <div className="card">
         <h2>Rick Sanchez</h2>
