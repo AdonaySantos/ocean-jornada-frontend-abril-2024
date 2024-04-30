a48b170eec2d9bbca14ebd8716a22aa49f91c165
Adonay Santos <adonay.rssantos@gmail.com>
2024-04-30 22:15:14 +0000

    ligando ao back-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,46 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  const item1 = {
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    tags: ['Status: Vivo', 'Espécie: Humana', 'Origem: Terra C-137']
-  }
+  // const item1 = {
+  //   name: 'Rick Sanchez',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+  //   tags: ['Status: Vivo', 'Espécie: Humana', 'Origem: Terra C-137']
+  // }
 
-  const item2 = {
-    name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
-    tags: ['Status: Vivo']
-  }
+  // const item2 = {
+  //   name: 'Morty Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+  //   tags: ['Status: Vivo']
+  // }
 
-  const item3 = {
-    name: 'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
-    tags: ['Espécie: Humana']
-  }
+  // const item3 = {
+  //   name: 'Summer Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+  //   tags: ['Espécie: Humana']
+  // }
 
-  const itens = [item1, item2, item3]
+  // const itens = [item1, item2, item3]
+
+  const [itens, setItens] = useState([])
+
+  const apiUrl = 'https://rickandmortyapi.com/api/character/'
+
+  useEffect(function (){
+    async function carregarDados(){
+      const response = await fetch(apiUrl)
+
+      const data = await response.json()
+
+      console.log(data)
+
+      const results = data.results
+      setItens(results)
+    }
+
+    carregarDados()
+  }, [])
 
   return (
     <>
