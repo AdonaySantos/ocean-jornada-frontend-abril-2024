b6036d3c27d8d9e7f958adcb8847140eeded6f1c
Adonay Santos <adonay.rssantos@gmail.com>
2024-05-02 23:31:10 +0000

    adição de comentários

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,22 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Create() {
-    function onSubmit(event) {
-        event.preventDefault()
+    const naviate = useNavigate()
 
+    async function onSubmit(event) {
+        event.preventDefault()
+        
+        //previne que o form saia da paina
         const name = event.target.name.value 
         const image = event.target.image.value 
-
+        
+        // Monta um objeto com os dados da personagem
         const data = {
             name,
             image
         }
-
+        
+        // Transforma um objeto em uma string JSON
         const json = JSON.stringify(data)
 
         console.log(data, json)
-
         
+        // Declara a URL da API para realização do POST
+        const apiUrl = 'https://ocean-jornada-backend-abril-2024.onrender.com/item'
+        
+        // Cria e executa a requisição HTTP para o endpoint POST
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: json
+        })
+
+        // Redireciona para a página principal
+        return navigate('/')
     }
     return (
         <>
@@ -27,7 +45,7 @@ export default function Create() {
                     <input type="text" id="name" name="name" placeholder="Digite o nome"></input>
                     <br />
 
-                    <label htmlFor="name">Image:</label>
+                    <label htmlFor="image">Image:</label>
                     <input type="text" id="image" name="image" placeholder="Digite a URL da imagem"></input>
                     <br />
 
